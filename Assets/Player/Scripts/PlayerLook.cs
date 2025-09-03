4c42813ae7b9ef2ecc8bd47ac74d9b1fa4aefdde
Toupak <vitello.th@gmail.com>
2025-09-03 02:09:59 +0200

    Added Recoil

--- Assets/Player/Scripts/PlayerLook.cs
+++ Assets/Player/Scripts/PlayerLook.cs
@@ -10,14 +10,49 @@ public class PlayerLook : MonoBehaviour
 
     public Transform orientation;
 
-    float xRotation;
-    float yRotation;
+    private float xRotation;
+    private float yRotation;
+
+    [SerializeField] private float cameraSpeedShootingSpread;
+
+    private float targetxOffset;
+    private float targetyOffset;
+
+    private float currentxOffset;
+    private float currentyOffset;
+
+    private float onStopShootxOffset;
+    private float onStopShootyOffset;
 
     void Start()
     {
         Mouse.current.WarpCursorPosition(new Vector2(Screen.width / 2, Screen.height / 2));
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        RifleShoot.OnPlayerShoot.AddListener((x, y) =>
+        {
+            currentxOffset = xRotation;
+            currentyOffset = yRotation;
+
+            targetxOffset = currentxOffset + x;
+            targetyOffset = currentyOffset + y;
+        });
+
+        RifleShoot.OnPlayerStartShooting.AddListener(() =>
+        {
+            onStopShootxOffset = 0;
+            onStopShootyOffset = 0;
+        });
+
+        RifleShoot.OnPlayerStopShooting.AddListener(() =>
+        {
+            currentxOffset = xRotation;
+            currentyOffset = yRotation;
+
+            targetxOffset = currentxOffset - onStopShootxOffset;
+            targetyOffset = currentyOffset - onStopShootyOffset;
+        });
     }
 
     void Update()
@@ -32,7 +67,23 @@ public class PlayerLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         //rotate cam & orientation
+        AddRecoilEffect();
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void AddRecoilEffect()
+    {
+        float previousxOffset = currentxOffset;
+        float previousyOffset = currentyOffset;
+        
+        currentxOffset = Mathf.MoveTowards(currentxOffset, targetxOffset, cameraSpeedShootingSpread * Time.deltaTime);
+        currentyOffset = Mathf.MoveTowards(currentyOffset, targetyOffset, cameraSpeedShootingSpread * Time.deltaTime);
+
+        xRotation += currentxOffset - previousxOffset;
+        yRotation += currentyOffset - previousyOffset;
+
+        onStopShootxOffset += currentxOffset - previousxOffset;
+        onStopShootyOffset += currentyOffset - previousyOffset;
     }
 }
--- Assets/Weapons/Rifle/RifleShoot.cs
+++ Assets/Weapons/Rifle/RifleShoot.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class RifleShoot : MonoBehaviour
 {
+    [HideInInspector] public static UnityEvent<float, float> OnPlayerShoot = new UnityEvent<float, float>();
+    [HideInInspector] public static UnityEvent OnPlayerStartShooting = new UnityEvent();
+    [HideInInspector] public static UnityEvent OnPlayerStopShooting = new UnityEvent();
+
     [SerializeField] private float firerate;
 
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform bulletSpawnPosition;
     [SerializeField] private float bulletVelocity;
+    [SerializeField] private float spreadIntensity;
 
     [SerializeField] private List<AudioClip> shootingSounds;
 
@@ -16,6 +23,12 @@ public class RifleShoot : MonoBehaviour
 
     void Update()
     {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            RegisterStartShooting();
+
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+            RegisterStopShooting();
+
         if (PlayerStateMachine.Instance.input.GetShootInput() && CanShoot())
             Shoot();
     }
@@ -32,10 +45,34 @@ public class RifleShoot : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().velocity = finalDirection * bulletVelocity;
 
+        OnPlayerShoot.Invoke(CalculateSpread(), CalculateSpread());
+
         ShootingSound();
 
         Destroy(bullet, 1f);
     }
+
+    private float CalculateSpread()
+    {
+        return Random.Range(-spreadIntensity, spreadIntensity);
+    }
+
+    private void RegisterStartShooting()
+    {
+        OnPlayerStartShooting.Invoke();
+    }
+
+    private void RegisterStopShooting()
+    {
+        OnPlayerStopShooting.Invoke();
+    }
+
+    //2x plus fort vers le haut - pas vers le bas - 2x moins fort en horizontal
+    //limite de temps ou ça arrête de monter
+    //vitesse différente aller/retour
+    //Ajouter "snapiness"
+    //Ajouter "ReturnSpeed"
+    //Ce script qui envoie la vitesse de caméra et pas l'autre
 
     private bool CanShoot()
     {
